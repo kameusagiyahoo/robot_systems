219cf6e685f91e987f675e2f99d414fda3f5a349
kameusagiyahoo <nywhatisbudou@yahoo.co.jp>
2026-09-06 11:27:20 +0900

    Allow asynchronous robot adapters

--- src/robot/robot_interface.js
+++ src/robot/robot_interface.js
@@ -1,1 +1,7 @@
-export class RobotInterface{connect(){throw new Error('not implemented')}getObservation(){throw new Error('not implemented')}sendAction(_action){throw new Error('not implemented')}disconnect(){throw new Error('not implemented')}}
+export class RobotInterface{
+  connect(){throw new Error('not implemented')}
+  getObservation(){throw new Error('not implemented')}
+  // Implementations may return a plain result or a Promise. Policy/Environment callers must await it.
+  sendAction(_action){throw new Error('not implemented')}
+  disconnect(){throw new Error('not implemented')}
+}
