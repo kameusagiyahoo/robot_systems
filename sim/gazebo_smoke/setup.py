ee54beb5dd8b14bd703803eb9d0887f4a98b87cb
kameusagiyahoo <nywhatisbudou@yahoo.co.jp>
2026-09-06 19:04:00 +0900

    Add Gazebo smoke package setup

---
+++ sim/gazebo_smoke/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'robot_systems_gazebo_smoke'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robot_systems',
+    maintainer_email='noreply@example.com',
+    description='Minimal Gazebo smoke environment for robot_systems.',
+    license='MIT',
+)
