5bb65ed15c8ac4c1ab513d66a36a296558fa384f
kameusagiyahoo <nywhatisbudou@yahoo.co.jp>
2026-09-06 11:27:07 +0900

    Extend PolicyInterface with environment context

--- src/policy/policy_interface.js
+++ src/policy/policy_interface.js
@@ -1,8 +1,13 @@
 export class PolicyInterface {
-  constructor(store, robot) {
+  constructor(store, robot, {environment=null,domainServices=null}={}) {
     this.store = store;
     this.robot = robot;
+    this.environment = environment;
+    this.domainServices = domainServices;
   }
+
+  getState(){return this.environment?.getState?.()||this.store?.state||null}
+  async sendAction(action){if(this.environment?.step)return await this.environment.step(action);return await Promise.resolve(this.robot?.sendAction?.(action))}
 
   async execute(_skill, _args = {}) {
     throw new Error('PolicyInterface.execute() must be implemented');
